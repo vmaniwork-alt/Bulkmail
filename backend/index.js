d6df36aebcde7b10f61038f6217964db5bfbe617
Mani V <vmani.work@gmail.com>
2025-12-20 13:20:32 +0530

    litle change in backend

--- backend/index.js
+++ backend/index.js
@@ -58,13 +58,9 @@ app.post("/sendmail", (req, res) => {
 })
 
 
-
-
- 
-
-
 })
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, () => {
-  console.log("server started");
-});
+app.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`)
+})
